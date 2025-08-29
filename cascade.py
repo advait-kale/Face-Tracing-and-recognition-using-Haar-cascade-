2923745b719c4ffe42f70fe1038fcb7641364e72
advait-kale <advaitkale9@gmail.com>
2025-08-29 11:44:47 +0530

    added the take 100 gray images and save function

--- cascade.py
+++ cascade.py
@@ -4,38 +4,42 @@ import os
 face_cascade = cv2.CascadeClassifier(r"c:\Advait\VS_Code\VS code 2.0\Face recognition\haarcascade_frontalface_default.xml")
 
 #variable declaration
-count = 0 #couning the images 
+global count #couning the images 
 
 cap = cv2.VideoCapture(0)
-while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-        #int arguments are the img then starting point of the rectange then the ending point of the rectange
-        #then the color of the rectange then the width of the rectangle
+def take_images():
+    while True:
+        count = 0
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+            #int arguments are the img then starting point of the rectange then the ending point of the rectange
+            #then the color of the rectange then the width of the rectangle
 
-        #extract face data
-        face = gray[y:y+h, x:x+h]
-        face = cv2.resize(face, (100,100))
+            #extract face data
+            face = gray[y:y+h, x:x+h]
+            face = cv2.resize(face, (100,100))
 
-        file_name = os.path.join("C:\Advait\VS_Code\VS code 2.0\Face recognition\Images\known", f"face_{count}.jpg")
-        cv2.imwrite(file_name, face)
-        count += 1
+            file_name = os.path.join("C:\Advait\VS_Code\VS code 2.0\Face recognition\Images\known", f"face_{count}.jpg")
+            cv2.imwrite(file_name, face)
+            count += 1
 
-        #  Stop after 100 images
-        # if count == 100:
-        #     break
-
-
-    cv2.imshow('img',img)#Displays the current frame (img) in a window titled 'img'
-    k = cv2.waitKey(30) & 0xff
-    if(k == 27):#Checks if the pressed key is ESC (ASCII value 27).
-        break
-    elif(count >= 100):
-        break
-cap.release()#Releases the webcam resource.
-cv2.destroyAllWindows()
+            #  Stop after 100 images
+            # if count == 100:
+            #     break
 
 
+        cv2.imshow('img',img)#Displays the current frame (img) in a window titled 'img'
+        k = cv2.waitKey(30) #& 0xff
+        if(k == 27):#Checks if the pressed key is ESC (ASCII value 27).
+            break
+        elif(count >= 100):
+            break
+    cap.release()#Releases the webcam resource.
+    cv2.destroyAllWindows()
+
+
+
+take_images()
